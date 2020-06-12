76accbdaed3d6033745f630059e408b16b3d92f3
Jonathan Allard <jonathan@allard.io>
2020-06-11 20:15:38 -0400

    Change eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,17 @@ module.exports = {
   env: {
     node: true
   },
+  plugins: ['coffee'],
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'eslint-plugin-coffee'
   },
   rules: {
+    "vue/require-render-return": "off",
+    "vue/return-in-computed-property": "off",
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
