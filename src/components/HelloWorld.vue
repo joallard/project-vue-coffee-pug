ce911edf62e786792fdbca4d5d6638c2462e17b4
Jonathan Allard <jonathan@allard.io>
2020-06-11 20:14:57 -0400

    HelloWorld: Put coffee syntax

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ foo }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -36,6 +36,10 @@ export default {
   props: {
     msg: String
   }
+
+  computed:
+    foo: -> 2
+    # bar: -> baz
 }
 </script>
 
